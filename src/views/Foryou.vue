8b7436bc5007377629774c7869bb61ac9983c084
ASoTiesto <cute17852337569@gmail.com>
2023-07-08 15:35:57 +0800

    將影片播放進度與進度條顯示同步

--- src/views/Foryou.vue
+++ src/views/Foryou.vue
@@ -4,16 +4,23 @@
                :src="currentVideo"
                controls
                autoplay
-               @ended="replayVideo"
+               @ended="replayVideo();handleVideoEnded();"
                @touchstart="handleTouchStart"
                @touchmove="handleTouchMove"
                @touchend="handleTouchEnd">
         </video>
 
-        <div class="progress-bar">
+        <!-- <div class="progress-bar">
             <div class="progress"
                  :style="{ width: progressBarWidth }"></div>
-        </div>
+        </div> -->
+
+        <input type="range"
+               min="0"
+               :max="videoDuration"
+               v-model="currentTime"
+               class="slider"
+               @input="seekVideo">
     </div>
 </template>
 
@@ -27,6 +34,8 @@ const videoPlayer = ref(null)
 const videos = ref([])
 const currentVideo = ref("")
 const progressBarWidth = ref("0%")
+const currentTime = ref(0)
+const videoDuration = ref(0)
 
 let touchStartY = 0
 let touchEndY = 0
@@ -51,26 +60,24 @@ onMounted(async () => {
         videos.value[currentVideoIndex].progress = parsedProgress.progress
     }
 
-
     await playVideo()
 
-    // 設置 progress bar 寬度監聽器
-    watch(videoPlayer, () => {
-        const currentTime = videoPlayer.value.currentTime
-        const duration = videoPlayer.value.duration
-        const progress = (currentTime / duration) * 100
-        progressBarWidth.value = `${progress}%`
+    watch(videoPlayer.value, () => {
+        videoDuration.value = videoPlayer.value.duration
     }, { immediate: true })
 
-
-    // 監聽 video 影片更新進度
+    // 監聽視頻時間更新事件
     videoPlayer.value.addEventListener('timeupdate', () => {
-        const currentTime = videoPlayer.value.currentTime
-        const duration = videoPlayer.value.duration
-        const progress = (currentTime / duration) * 100
+        currentTime.value = videoPlayer.value.currentTime
+        const progress = (videoPlayer.value.currentTime / videoPlayer.value.duration) * 100
         progressBarWidth.value = `${progress}%`
+
+        // console.log(progressBarWidth.value)
     })
 
+    videoPlayer.value.addEventListener('loadedmetadata', () => {
+        videoDuration.value = videoPlayer.value.duration
+    })
 })
 
 const initTable = async () => {
@@ -134,6 +141,20 @@ const replayVideo = () => {
     videoPlayer.value.currentTime = 0
     videoPlayer.value.play()
 }
+
+const seekVideo = () => {
+    videoPlayer.value.currentTime = currentTime.value
+}
+
+const handleVideoEnded = () => {
+    // 等待影片元數據載入完成
+    videoPlayer.value.addEventListener('loadedmetadata', () => {
+        videoDuration.value = videoPlayer.value.duration
+
+        // 將進度條寬度設置為 100%
+        progressBarWidth.value = "100%"
+    })
+}
 </script>
 
 <style>
@@ -145,7 +166,7 @@ const replayVideo = () => {
     }
 }
 
-.progress-bar {
+/* .progress-bar {
     background-color: darkred;
     height: 8px;
     margin-top: 10px;
@@ -157,5 +178,35 @@ const replayVideo = () => {
     height: 100%;
     width: 0;
     transition: width 0.3s ease-in-out;
+} */
+
+input.slider {
+    -webkit-appearance: none;
+    width: 100%;
+    height: 8px;
+    border-radius: 4px;
+    background: #eaeaea;
+    outline: none;
+    opacity: 1;
+    -webkit-transition: 0.2s;
+    transition: opacity 0.2s;
+}
+
+input.slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    background: #007bff;
+    cursor: pointer;
+}
+
+input.slider::-moz-range-thumb {
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    background: #007bff;
+    cursor: pointer;
 }
 </style>
